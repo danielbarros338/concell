5e8d122092093f2f0e42ee953a141267d045d67d
Daniel Barros <danielbarros338@gmail.com>
2022-04-28 22:09:50 -0300

    Adicionado opcoes de menu

--- src/components/shared/BaseInput.vue
+++ src/components/shared/BaseInput.vue
@@ -1,5 +1,9 @@
 <template>
-  <q-input dense standout="bg-grey-9" color="grey-9" />
+  <q-input dense standout="bg-grey-9" color="grey-9">
+    <template v-slot:append>
+      <slot name="append"></slot>
+    </template>
+  </q-input>
 </template>
 
 <script>
--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>DASHBOARD</div>
+  <q-layout>
+    <q-header class="bg-amber-7 header__container">
+      <q-toolbar>
+        <q-toolbar-title> Bem vindo {{ name }} </q-toolbar-title>
+        <BaseInput>
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </BaseInput>
+      </q-toolbar>
+      <q-tabs class="tab">
+        <q-tab><q-icon name="fa-solid fa-wrench" />Nova manutenção</q-tab>
+        <q-tab><q-icon name="fa-solid fa-user" />Cadastros</q-tab>
+        <q-tab><q-icon name="fa-solid fa-gear" />Manutenções</q-tab>
+        <q-tab><q-icon name="fa-solid fa-mobile" />Estoque de peças</q-tab>
+      </q-tabs>
+    </q-header>
+  </q-layout>
 </template>
 
 <script>
 export default {
   name: "DashboardLayout",
+  data() {
+    return {
+      name: "Daniel",
+    };
+  },
 };
 </script>
+
+<style scoped>
+/* Containers */
+.header__container {
+  color: var(--color-primary);
+}
+/* Elements */
+</style>
