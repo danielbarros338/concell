06c7b9d932e333fe5a2c37f387e4455a04382f8a
Daniel Barros <danielbarros338@gmail.com>
2022-05-23 22:25:20 -0300

    Implementado tabela de exibicao de dados

--- src/components/stock/StockHistoryComponent.vue
+++ src/components/stock/StockHistoryComponent.vue
@@ -1,3 +1,64 @@
 <template>
-  <h5>StockHistoryComponent</h5>
+  <div class="table__container">
+    <q-table
+      title="Saídas"
+      :rows="rows"
+      :columns="columns"
+      row-key="os"
+      dark
+      class="table"
+    />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "StockHistoryComponent",
+  data() {
+    return {
+      columns: [
+        {
+          name: "os",
+          required: true,
+          label: "Ordem de Serviço",
+          align: "left",
+          field: (row) => row.os,
+          format: (val) => `${val}`,
+          sortable: true,
+        },
+        {
+          name: "part",
+          align: "center",
+          label: "Peça",
+          field: "part",
+          sortable: true,
+        },
+      ],
+      rows: [
+        {
+          os: 1121212,
+          part: "Câmera LG",
+        },
+        {
+          os: 232132,
+          part: "Frontal iPhone",
+        },
+        {
+          os: 3542456,
+          part: "Processador Xiaomi",
+        },
+        {
+          os: 4455462341,
+          part: "Placa Samsung",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.table__container {
+  width: 100%;
+}
+</style>
